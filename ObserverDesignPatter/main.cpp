b851ffb2b6e6e3755b4d4dd40a8bceadef220312
Harold Serrano <haroldserrano@Harolds-MacBook-Pro.local>
2013-03-30 12:48:52 -0700

    Initial Commit

---
+++ ObserverDesignPatter/main.cpp
@@ -0,0 +1,18 @@
+//
+//  main.cpp
+//  ObserverDesignPatter
+//
+//  Created by Harold Serrano on 3/30/13.
+//  Copyright (c) 2013 Harold Serrano. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[])
+{
+
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
+
