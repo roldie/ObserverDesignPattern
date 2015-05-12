0e540d24e1589727ce9211756d4a63631538e69f
Harold Serrano <harold.serrano@gmail.com>
2015-05-11 21:51:16 -0700

    Updated the copyright

--- ObserverDesignPatter/Observer.cpp
+++ ObserverDesignPatter/Observer.cpp
@@ -3,7 +3,7 @@
 //  ObserverDesignPatter
 //
 //  Created by Harold Serrano on 3/30/13.
-//  Copyright (c) 2013 Harold Serrano. All rights reserved.
+//  Copyright (c) 2015 www.cgdemy.com All rights reserved.
 //
 
 #include "Observer.h"
