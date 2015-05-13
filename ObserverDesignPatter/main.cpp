e2537c199060a3b7500a24b1c7aef00892c8d4fa
Harold Serrano <harold.serrano@gmail.com>
2015-05-12 21:09:58 -0700

    added comments

--- ObserverDesignPatter/main.cpp
+++ ObserverDesignPatter/main.cpp
@@ -14,16 +14,16 @@
 int main(int argc, const char * argv[])
 {
 
-    //create our newsStation. NOTE how I'm coding to the interface "subject", not to the implementation "newsStation"
+    //1. create our newsStation. NOTE how I'm coding to the interface "subject", not to the implementation "newsStation"
     Subject *newsStation=new NewsStation();
     
-    //creating our newsReader class. When the constructor gets called, the newsReader class is register with the newsStation
+    //2. creating our newsReader class. When the constructor gets called, the newsReader class is register with the newsStation
     Observer *newsReader=new NewsReader(newsStation);
 
-    //creating our neighborReader class. When the constructor gets called, the neighborReader class is register with the newsStation
+    //3. creating our neighborReader class. When the constructor gets called, the neighborReader class is register with the newsStation
     Observer *neighborReader=new NeighborReader(newsStation);
     
-    //the newsStation notifies all observers.
+    //4. the newsStation notifies all observers.
     newsStation->notifyObserver();
     
     return 0;
